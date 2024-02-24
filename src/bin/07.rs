9ea127135189f7770bce661a7c7f9baf4f0ebdcb
Ethan Nguyen <manhnguyend8@gmail.com>
2024-02-24 00:03:38 +0000

    Finish day 7

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,35 +1,91 @@
 advent_of_code::solution!(7);
 
-fn parse_hand(hand: &str, mapping: &[char; 13]) -> u64 {
-    hand.chars()
-        .map(|c| mapping.iter().position(|&x| x == c).unwrap())
-        // TODO: Change the init to classfied hand rank number
-        .fold(0, |acc, x| acc * 100 + x as u64)
+fn classify_hand(card_ranks: &Vec<usize>) -> usize {
+    // We're using an array length 15 to store frequencies of card ranks.
+    let mut freq = [0; 15];
+    card_ranks.iter().for_each(|&c| {
+        freq[c] += 1;
+    });
+    // Turning off the count of jokers in case its value is 1
+    let jokers = freq[1];
+    freq[1] = 0;
+    freq.sort_unstable_by(|&a, &b| b.cmp(&a));
+    // Jokers will always become whichever card is highest in frequency
+    freq[0] += jokers;
+
+    // Encoding the hand type as an integer by looking at the top 5 rank frequencies
+    let mut hand_type = 0;
+    for f in freq.iter().take(5) {
+        hand_type = (hand_type << 4) | f;
+    }
+    hand_type
+}
+
+fn parse_hand(hand: &str, j_value: usize) -> usize {
+    // Convert cards to its rank first
+    let card_ranks: Vec<usize> = hand
+        .bytes()
+        .map(|b| match b {
+            b'A' => 14,
+            b'K' => 13,
+            b'Q' => 12,
+            b'J' => j_value,
+            b'T' => 10,
+            _ => (b - b'0') as usize,
+        })
+        .collect();
+    // Get hand rank first
+    let mut hand_value = classify_hand(&card_ranks);
+    // Continue to bit shift to encode the rank info into the hand value
+    for r in card_ranks {
+        hand_value = (hand_value << 4) | r;
+    }
+    hand_value
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    const MAPPING: [char; 13] = [
-        '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A',
-    ];
-    let results = input
+    let mut data: Vec<(usize, u32)> = input
         .lines()
         .map(|line| {
-            let (hand_value, bid_value) = {
-                let mut parts = line.split_ascii_whitespace();
-                (
-                    parse_hand(parts.next().unwrap(), &MAPPING),
-                    parts.next().unwrap().parse::<u64>().unwrap(),
-                )
-            };
-            println!("hand: {}, bid: {}", hand_value, bid_value);
-            (hand_value, bid_value)
+            // First part is the hand, second part is the bid
+            let mut parts = line.split_ascii_whitespace();
+            (
+                parse_hand(parts.next().unwrap(), 11),
+                parts.next().unwrap().parse::<u32>().unwrap(),
+            )
         })
-        .collect::<Vec<(u64, u64)>>();
-    Some(6440)
-}
+        .collect();
+    // sort by hand value to use the rank later
+    data.sort_unstable_by(|a, b| a.0.cmp(&b.0));
 
+    Some(
+        data.iter()
+            .enumerate()
+            .map(|(i, &(_, bid))| ((i as u32) + 1) * bid)
+            .sum(),
+    )
+}
+// Everything the same as part 1, we just change the joker value to 1
 pub fn part_two(input: &str) -> Option<u32> {
-    Some(5905)
+    let mut data: Vec<(usize, u32)> = input
+        .lines()
+        .map(|line| {
+            let mut parts = line.split_ascii_whitespace();
+            (
+                parse_hand(parts.next().unwrap(), 1),
+                parts.next().unwrap().parse::<u32>().unwrap(),
+            )
+        })
+        .collect();
+    // sort by hand value to use the rank later
+    data.sort_unstable_by(|a, b| a.0.cmp(&b.0));
+
+    Some(
+        data.iter()
+            .enumerate()
+            .map(|(i, &(_, bid))| ((i as u32) + 1) * bid)
+            .sum(),
+    )
 }
 
 #[cfg(test)]
